a3fc074915a1b671493d66d159a4954a021c7ebe
Ecorous <ecorous@pridecraft.gay>
2023-07-04 20:39:58 +0100

    feat: code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,101 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	var new_projects: any[] = [];
+	var offset = 0;
+	var step = 100;
+	var projects: any[] = [];
+	var visible = true;
+	var x: HTMLDivElement;
+	var loadMoreButton: HTMLButtonElement;
+	var disabled = false;
+
+	let loadNext = async () => {
+		let response = await fetch(
+			'https://api.modrinth.com/v2/search?limit=' +
+				step +
+				'&offset=' +
+				offset +
+				'&facets=[["project_type:mod"]]&filters=categories="quilt" AND (NOT categories="fabric")'
+		);
+		let body = await response.json();
+		body.hits.forEach((project) => {
+			console.log(project);
+			projects.push(project);
+		});
+		if (body.hits.length == 0) {
+			loadMoreButton.disabled = true;
+			disabled = true;
+			return;
+		}
+		visible = false;
+		setTimeout(() => (visible = true), 100);
+		offset += step;
+	};
+
+	onMount(async () => {
+		loadNext();
+	});
+</script>
+
+<svelte:head>
+	<title>Quilt-only mods (on modrinth)</title>
+</svelte:head>
+<h1>Quilt-only mods from Modrinth</h1>
+<div bind:this={x}>
+	{#if visible}
+		{#each projects as project}
+			<div
+				on:click={() => {
+					window.open('https://modrinth.com/project/' + project.project_id, '_blank');
+				}}
+				class="project"
+			>
+				<div class="image-text-align">
+					<div class="image-align">
+						<img alt={project.title + ' logo'} src={project.icon_url} />
+					</div>
+					<div class="text-align">
+						<h2>{project.title}</h2>
+						<p>{project.description}</p>
+					</div>
+				</div>
+			</div>
+		{/each}
+	{/if}
+	<button bind:this={loadMoreButton} on:click={loadNext}>Load More</button>
+	{#if disabled}
+		<p style="font-weight: bolder; text-color: red;">No more results.</p>
+	{/if}
+</div>
+
+<style>
+	div.project {
+		border-radius: 15px;
+		background-color: grey;
+		width: max-content;
+		padding: 5px;
+		margin-bottom: 10px;
+		min-width: 60rem;
+		max-width: 60rem;
+		cursor: pointer;
+	}
+	div.image-align > img {
+		width: 96px;
+		height: 96px;
+		border-radius: 0.75rem;
+		border-right: 8rem;
+	}
+	div.image-align {
+		border-right: 8rem;
+		max-width: 96px;
+	}
+	div.image-text-align {
+		display: flex;
+	}
+	div.text-align {
+		margin-left: 0.75rem;
+	}
+	div.text-align > h2:hover {
+		text-decoration: underline;
+	}
+</style>
